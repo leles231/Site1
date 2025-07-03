af45793d635e46c4e26fd5c531e3847584bd83eb
leles231 <raquelleles231@gmail.com>
2025-07-03 00:31:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,31 +31,3 @@
     }, 3000);
     return false;
   }
-<script>
-  const toggle = document.getElementById('menu-toggle');
-  const menu = document.getElementById('menu-lateral');
-
-  toggle.addEventListener('click', () => {
-    toggle.classList.toggle('ativo');
-    menu.classList.toggle('ativo');
-  });
-
-  document.addEventListener('click', (e) => {
-    if (!menu.contains(e.target) && !toggle.contains(e.target)) {
-      menu.classList.remove('ativo');
-      toggle.classList.remove('ativo');
-    }
-  });
-
-  function enviarFormulario(event) {
-    event.preventDefault();
-    document.getElementById("mensagem-enviada").style.display = "block";
-
-    setTimeout(() => {
-      document.getElementById("mensagem-enviada").style.display = "none";
-      document.querySelector(".formulario").reset();
-    }, 3000);
-
-    return false;
-  }
-</script>
